5811166972ae588509b03363ecf75d23f72ee567
lzfprivate <1656194324@qq.com>
2024-12-06 22:54:45 +0800

    user define shader

--- OpenGL/OpenGL/Application.cpp
+++ OpenGL/OpenGL/Application.cpp
@@ -2,7 +2,61 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+/// <summary>
+/// 编译着色器
+/// </summary>
+/// <param name="vertexShader"></param>
+/// <param name="type">着色器类型 </param>
+/// <returns></returns>
+static int CompileShader(unsigned int type,
+    const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
 
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(sizeof(char) * length);
+        glGetShaderInfoLog(id, length, &length, message);
+        
+        glDeleteShader(id);
+        return 0;
+    }
+
+
+    return id;
+}
+/// <summary>
+/// 
+/// </summary>
+/// <param name="vertexShader">顶点着色器</param>
+/// <param name="fragmentShader">片段着色器</param>
+/// <returns>标识符</returns>
+static unsigned int CreateShader(const std::string& vertexShader, 
+    const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+
+}
 
 int main(void)
 {
@@ -52,12 +106,37 @@ int main(void)
     //最后一个参数:当前只有坐标一个属性，所以当前的偏移量为0
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    //glsl 指定顶点着色器
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;"
+        "\n"
+        "void main(void)\n"
+        "{\n"
+        "gl_Position = position;\n"
+        "}\n";
+    //glsl 指定片段着色器
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;"          
+        "\n"
+        "void main(void)\n"
+        "{\n"
+        "color = vec4(1.0 , 0.8 , 0.3 , 1.0);\n"    //rgba
+        "}\n";
+
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
+
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        //绘制数据，运行不会有显示 没有设置着色器
+        //绘制数据，可以显示图形，在上面我们手动设置了顶点着色器和片段着色器
         glDrawArrays(GL_TRIANGLES, 0, 3);       
 
         /* Swap front and back buffers */
@@ -66,7 +145,7 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    glDeleteProgram(shader);
     glfwTerminate();
     return 0;
 }
